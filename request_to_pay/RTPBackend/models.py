953d3380229cae4dbe0b68d8bb2f30a8df29aef1
Mystery3051 <alexandr.job@yahoo.com>
2019-10-26 18:47:23 -0400

    Connected DjangoREST and created a user model.

--- request_to_pay/RTPBackend/models.py
+++ request_to_pay/RTPBackend/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
 
 class Invoice(models.Model):
     STATUS_CHOICES = [("A", "Active"), ("P", "Paid"), ("D", "Delivered")]
--- request_to_pay/request_to_pay/settings.py
+++ request_to_pay/request_to_pay/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'RTPBackend',
 ]
 
@@ -99,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = "RTPBackend.User"
 
 
 # Internationalization
