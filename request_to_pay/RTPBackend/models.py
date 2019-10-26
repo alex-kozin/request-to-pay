7184db0022a46af691295fbbd9b94f82ef503f6a
Jehan Vakharia <jehan.vakharia@mail.utoronto.ca>
2019-10-26 16:46:14 -0400

    "Updated models.py to include our four models."

--- request_to_pay/RTPBackend/models.py
+++ request_to_pay/RTPBackend/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+class Invoice(models.Model):
+    STATUS_CHOICES = [("A", "Active"), ("P", "Paid"), ("D", "Delivered")]
+    price = models.DecimalField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
+    customer_id = models.ForeignKey("UserProfile")
+
+
+class Order(models.Model):
+    invoice_id = models.ForeignKey("Invoice")
+    ##ALTERNTAIVELY: item_type
+    item_id = models.ForeignKey("Item")
+    quantity = models.IntegerField()
+    price = models.DecimalField()
+    driver_id = models.ForeignKey("UserProfile")
+
+
+class Item(models.Model):
+    name = models.CharField()
+    price = models.DecimalField()
+
+
+class UserProfile(models.Model):
+    USER_CHOICES = [("S", "Suppliers"), ("C", "Customer"), ("D", "Driver")]
+    name = models.CharField
+    address =  models.TextField(blank=True)
+    user_type = models.CharField(choices=USER_CHOICES, max_length=1)
+    status = models.BooleanField()
