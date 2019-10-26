d9781a44c7f477029fd244db55ab76a3e8d72c41
Jehan Vakharia <jehan.vakharia@mail.utoronto.ca>
2019-10-26 17:29:22 -0400

    "Fixed bugs"

--- request_to_pay/RTPBackend/models.py
+++ request_to_pay/RTPBackend/models.py
@@ -4,16 +4,16 @@ class Invoice(models.Model):
     STATUS_CHOICES = [("A", "Active"), ("P", "Paid"), ("D", "Delivered")]
     price = models.DecimalField(decimal_places=2, max_digits=12)
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
-    customer_id = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    customer = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
 
 
 class Order(models.Model):
-    invoice_id = models.ForeignKey("Invoice", on_delete=models.CASCADE)
+    invoice = models.ForeignKey("Invoice", on_delete=models.CASCADE)
     ##ALTERNTAIVELY: item_type
-    item_id = models.ForeignKey("Item", on_delete=models.CASCADE)
+    item = models.ForeignKey("Item", on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=12)
-    driver_id = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    driver = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
 
 
 class Item(models.Model):
@@ -23,7 +23,7 @@ class Item(models.Model):
 
 class UserProfile(models.Model):
     USER_CHOICES = [("S", "Suppliers"), ("C", "Customer"), ("D", "Driver")]
-    name = models.CharField
+    name = models.CharField( max_length=50, null=True)
     address = models.TextField(blank=True)
     user_type = models.CharField(choices=USER_CHOICES, max_length=1)
     status = models.BooleanField()
