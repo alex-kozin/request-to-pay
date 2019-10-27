9573bc6b3dd32caa9c1cf595dfadf755bc79487a
Mystery3051 <alexandr.job@yahoo.com>
2019-10-26 23:51:01 -0400

    Further decoupled userprofile functionality from RTPBackend app into userapi app
    NOTE: any userprofile objects created before migration 0004_userprofile_user will result in a conflict.
    Solution: manually delete these objects and create them after applying the abovementioned migration.

--- request_to_pay/RTPBackend/models.py
+++ request_to_pay/RTPBackend/models.py
@@ -5,26 +5,17 @@ class Invoice(models.Model):
     STATUS_CHOICES = [("A", "Active"), ("P", "Paid"), ("D", "Delivered")]
     price = models.DecimalField(decimal_places=2, max_digits=12)
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
-    customer = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    customer = models.ForeignKey("userapi.UserProfile", on_delete=models.CASCADE)
 
 
 class Order(models.Model):
     invoice = models.ForeignKey("Invoice", on_delete=models.CASCADE)
-    ##ALTERNTAIVELY: item_type
     item = models.ForeignKey("Item", on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=12)
-    driver = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    driver = models.ForeignKey("userapi.UserProfile", on_delete=models.CASCADE)
 
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(decimal_places=2, max_digits=12)
-
-
-class UserProfile(models.Model):
-    USER_CHOICES = [("S", "Suppliers"), ("C", "Customer"), ("D", "Driver")]
-    name = models.CharField( max_length=50, null=True)
-    address = models.TextField(blank=True)
-    user_type = models.CharField(choices=USER_CHOICES, max_length=1)
-    status = models.BooleanField()
--- request_to_pay/userapi/models.py
+++ request_to_pay/userapi/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(blank=True, null=True, max_length=50)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+
+class UserProfile(models.Model):
+    USER_CHOICES = [("S", "Suppliers"), ("C", "Customer"), ("D", "Driver")]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+    name = models.CharField( max_length=50, null=True)
+    address = models.TextField(blank=True)
+    user_type = models.CharField(choices=USER_CHOICES, max_length=1)
+    status = models.BooleanField()
