6c6c551658b5706ca377494377f71a257e8d370d
Mystery3051 <alexandr.job@yahoo.com>
2019-10-27 23:46:34 -0400

    Added item creation functionality.

--- request_to_pay/RTPBackend/api_views.py
+++ request_to_pay/RTPBackend/api_views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ValidationError
+from rest_framework.generics import ListAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ItemSerializer
 from .models import Item
@@ -9,3 +10,17 @@ class ItemList(ListAPIView):
     serializer_class = ItemSerializer
     filter_backends = (DjangoFilterBackend, )
     filter_fields = ('id', )
+
+
+class ItemCreate(CreateAPIView):
+    serializer_class = ItemSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            price = request.data.get("price")
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({"price": "Must be above $0.0"})
+        except:
+            raise ValidationError({"price": "A valid number is required"})
+        return super().create(request, *args, **kwargs)
+
--- request_to_pay/RTPBackend/urls.py
+++ request_to_pay/RTPBackend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import api_views
 
 urlpatterns = [
-    path('items/', api_views.ItemList.as_view())
+    path('items/', api_views.ItemList.as_view()),
+    path('items/new', api_views.ItemCreate.as_view()),
 ]
