992e4c128ce92a723e4d8a48f43af09b5d47483d
Mystery3051 <alexandr.job@yahoo.com>
2019-10-28 20:34:31 -0400

    Revert ""Fixed model keys"" Pulled the project with no migrations applied.
    This reverts commit 8adb6522789e7fe7ca38e6dc182a3acd2e2c2288.

--- request_to_pay/RTPBackend/models.py
+++ request_to_pay/RTPBackend/models.py
@@ -6,7 +6,6 @@ class Invoice(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=12)
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     customer = models.ForeignKey("userapi.UserProfile", on_delete=models.CASCADE)
-    driver = models.ForeignKey("userapi.UserProfile", on_delete=models.CASCADE)
 
 
 class Order(models.Model):
@@ -14,6 +13,7 @@ class Order(models.Model):
     item = models.ForeignKey("Item", on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(decimal_places=2, max_digits=12)
+    driver = models.ForeignKey("userapi.UserProfile", on_delete=models.CASCADE)
 
 
 class Item(models.Model):
