fba54aec00d812aeb282707b19d360df364c2013
Oliver Daniel <oliver.daniel@student.tdsb.on.ca>
2019-11-21 10:27:42 -0500

    Bring branch up to date

--- request_to_pay/userapi/urls.py
+++ request_to_pay/userapi/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+<<<<<<< Updated upstream
 from rest_framework.authtoken import views as drf_views
 from . import views
 from . import api_views
@@ -8,6 +9,12 @@ urlpatterns = [
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('auth/', drf_views.obtain_auth_token, name='auth'),
 
+=======
+from . import api_views, views
+
+urlpatterns = [
+    path('', views.whoami),
+>>>>>>> Stashed changes
     path('users/', api_views.UserProfileList.as_view()),
     path('users/<int:id>/', api_views.UserProfileRetrieveUpdate.as_view()),
 ]
