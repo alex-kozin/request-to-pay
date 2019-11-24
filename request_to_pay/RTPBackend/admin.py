d9c88916aa24679780ecfb561c33dd7d25a24e46
Alex Kozin <alexandr.job@yahoo.com>
2019-11-23 21:12:48 -0500

    Revert "Feature: Email Registration"

--- request_to_pay/RTPBackend/admin.py
+++ request_to_pay/RTPBackend/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from . import models
-from userapi import models as user_models
-# Register your models here.
 
-admin.site.register(user_models.User)
-admin.site.register(user_models.UserProfile)
+# Register your models here.
