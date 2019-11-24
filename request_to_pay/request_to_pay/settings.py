c46e2242424ad4bd3aa6e091818a4cb616384572
Mystery3051 <alexandr.job@yahoo.com>
2019-11-24 14:10:27 -0500

    Fixed migrations and DB settings.

--- request_to_pay/request_to_pay/settings.py
+++ request_to_pay/request_to_pay/settings.py
@@ -158,7 +158,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 # Internationalization
