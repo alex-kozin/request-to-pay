7836acd04e382c10b00670925a876841c93c530a
Mystery3051 <alexandr.job@yahoo.com>
2019-11-27 20:22:55 -0500

    No more AutoSchema that breaks the code.

--- request_to_pay/request_to_pay/settings.py
+++ request_to_pay/request_to_pay/settings.py
@@ -158,7 +158,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ] + (['rest_framework.authentication.SessionAuthentication'] if DEBUG else []),
-     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 # Internationalization
