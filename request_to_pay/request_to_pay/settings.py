38850980f59200e50e020c38909a3d32bb404fca
Mystery3051 <alexandr.job@yahoo.com>
2019-11-30 15:57:38 -0500

    Added Heroku adapter.

--- request_to_pay/request_to_pay/settings.py
+++ request_to_pay/request_to_pay/settings.py
@@ -44,12 +44,14 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'userapi',
     'RTPBackend',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
